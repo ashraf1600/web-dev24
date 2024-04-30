1dd3f52684bb2e62f6587fcdada028e587e79a9e
ashraf1600 <147105632+ashraf1600@users.noreply.github.com>
2024-04-30 16:06:26 +0600

    Add files via upload

---
+++ aaa.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    string s="0000";
+    string n;
+    cin>>n;
+    for (int i=3;i>=0;i--)
+    {
+       if(s[i])
+    }
+}
+
+
+
